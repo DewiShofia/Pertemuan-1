98b0e83dc3938d8b7988ba9fc7d551eecc9699ff
DewiShofia <dewishofiaturrohmah@gmail.com>
2025-09-05 23:29:01 +0700

    perbaruan data perbandingan

--- Hello.py
+++ Hello.py
@@ -1,23 +1,24 @@
-# String
-nama = 'Dewi'
-# Int
-umur = 25
-# Float
-tinggi = 155.2
-# Bolean 
-statusmenikah = False
-# Aritmatika
-a = 1
-b = 2.3
-c = a + b
-statusmenikahString = 'Sudah Kawin' if statusmenikah else 'Belum Kawin'
+# # String
+# nama = 'Dewi'
+# # Int
+# umur = 25
+# # Float
+# tinggi = 155.2
+# # Bolean 
+# statusmenikah = False
+# # Aritmatika
+# a = 1
+# b = 2.3
+# c = a + b
+# statusmenikahString = 'Sudah Kawin' if statusmenikah else 'Belum Kawin'
 
-print('Hello World')
-print (f'nama: {nama}')
-print (umur)
-print (tinggi)
-print (f'status: {statusmenikahString}')
-print (c)
-
+# print('Hello World')
+# print (f'nama: {nama}')
+# print (umur)
+# print (tinggi)
+# print (f'status: {statusmenikahString}')
+# print (c)
+nama = input ("Nama: ")
+umur = int
 
 
